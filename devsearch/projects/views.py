7cab83ab0210fc19c7bc0ef2ab5c121a8026ada1
JieNan <nan.j@northeastern.edu>
2023-06-22 21:56:42 -0700

    update new

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,35 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project
+from .forms import ProjectForm
 
-projectsLists = [
-    {
-        'id':'1',
-        'title': "Ecommerce Website",
-        'description':'Fully functional ecommerce website'
-    },
-    {
-        'id':'2',
-        'title':'Protfolio Website',
-        'description':'This was a project where I build out my portfolio'
-    },
-    {
-        'id':'3',
-        'title':'Social Network',
-        'description':'Awesome open source project I am still working on'
-    },
-]
 def projects(request):
     #msg = 'Hello, you are on the projects page'
     #'message' is key, msg is variable name
     #return render(request, 'projects/projects.html',{'message': msg})
-    page = 'projects'
-    number = 10
-    context  = {'page': page,'number': number,'projects': projectsLists}
+    # page = 'projects'
+    # number = 10
+    projects = Project.objects.all()
+    context  = {'projects': projects}
     return render(request,'projects/projects.html',context)
 
 def project(request,pk):
-    projectObj = None
-    for i in projectsLists:
-        if i['id'] == pk:
-            projectObj = i
+    projectObj = Project.objects.get(id=pk)
     return render(request, 'projects/single-project.html',{'project': projectObj})
+
+def createProject(request):
+    form = ProjectForm()
+    context = {'form': form}
+    return render(request,"projects/project_form.html",context);
